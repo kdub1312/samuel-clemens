442eb4c6f7607d86306ed3f94c982df8da325543
Kevins mac Air <kwagner1312@gmail.com>
2020-01-03 05:55:29 -0600

    gulp related files

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,69 @@
 const
   // modules
   gulp = require('gulp'),
-
+  newer = require('gulp-newer'),
+  imagemin = require('gulp-imagemin'),
+  htmlclean = require('gulp-htmlclean'),
+      
   // development mode?
-  devBuild = (process.env.NODE_ENV !== 'production'),
+//  devBuild = (process.env.NODE_ENV !== 'production'),
+
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  terser = require('gulp-terser'),
+  stripdebug = require('gulp-strip-debug'),
+  sourcemaps = require('gulp-sourcemaps'),
+  gulpless = require('gulp-less'),
+  gulpautoprefixer = require('gulp-autoprefixer'),
 
   // folders
   src = 'src/',
   build = 'build/';
+
+// image processing
+function images() {
+
+  const out = build + 'images/';
+
+  return gulp.src(src + 'images/**/*')
+    .pipe(newer(out))
+    .pipe(gulp.dest(out));
+
+};
+exports.images = images;
+
+// HTML processing
+function html() {
+  const out = build + 'html/';
+
+  return gulp.src(src + 'html/**/*')
+    .pipe(newer(out))
+    .pipe(htmlclean())
+    .pipe(gulp.dest(out));
+}
+exports.html = gulp.series(images, html);
+
+// JavaScript processing
+function js() {
+
+  return gulp.src(src + 'js/**/*')
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
+    .pipe(deporder())
+    .pipe(concat('main.js'))
+    .pipe(stripdebug ? stripdebug() : noop())
+    .pipe(terser())
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(gulp.dest(build + 'js/'));
+
+}
+exports.js = js;
+
+function styles() {
+    
+    return gulp.src(src + 'less/**/*')
+    .pipe(gulpless())
+    .pipe(gulpautoprefixer())
+    .pipe(gulp.dest(build + 'css/'));
+    
+}
+exports.styles = styles;
---
+++ src/js/scroll-effect.js
@@ -0,0 +1,1 @@
+var secondOne = 'second variable';
