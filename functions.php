b7949dd0c780201a1dba671392c682433f7e64e2
Kevins mac Air <kwagner1312@gmail.com>
2020-02-02 06:26:57 -0600

    added custom post template for web dev cat

--- functions.php
+++ functions.php
@@ -20,33 +20,8 @@ function twentysixteen_fonts_url() {
 	return $fonts_url;
 }
 
-/*
-* Define a constant path to our single template folder
-*/
-define(SINGLE_PATH, TEMPLATEPATH . '/single');
- 
-/**
-* Filter the single_template with our custom function
-*/
-add_filter('single_template', 'my_single_template');
- 
-/**
-* Single template function which will choose our template
-*/
-function my_single_template($single) {
-global $wp_query, $post;
- 
-/**
-* Checks for single template by category
-* Check by category slug and ID
-*/
-foreach((array)get_the_category() as $cat) :
- 
-if(file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
- 
-elseif(file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
- 
-endforeach;
-}
+function webdev_cat_styles() {
+    wp_register_style('webdev_stylesheet', get_stylesheet_directory_uri() . '/css/webdev-styles.css');
+    wp_enqueue_style('webdev_stylesheet');
+    }
+    add_action( 'wp_enqueue_scripts', 'webdev_cat_styles' ); 
