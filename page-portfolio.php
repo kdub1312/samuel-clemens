e7cc0de0c95c0a494ab5fda8ad759d67695c9591
kevin <kwagner1312@gmail.com>
2020-09-20 20:55:48 -0500

    add content-portfolio.php

--- page-portfolio.php
+++ page-portfolio.php
@@ -8,7 +8,7 @@
 			the_post();
 
 			// Include the page content template.
-			get_template_part( 'template-parts/content', 'page' );
+			get_template_part( 'template-parts/content', 'portfolio' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
---
+++ template-parts/content-portfolio.php
@@ -0,0 +1,1 @@
+<h2>hello from the content-portfolio.php file</h2>
